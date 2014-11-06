f0a76b0993d43c81f99cf999af73d316680b8897
romdim <rom-dim@hotmail.com>
2014-11-06 15:13:06 +0200

    Tried REST Controller by yii and..
    commented it out to go with swagger-php :)

---
+++ controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+///**
+// * Created by PhpStorm.
+// * User: Romanos-Dimokritos
+// * Date: 6/11/2014
+// * Time: 13:22
+// */
+//
+//namespace app\controllers;
+//
+//use yii\rest\ActiveController;
+//
+//class UserController extends ActiveController
+//{
+//	public $modelClass = 'app\models\User';
+//}
