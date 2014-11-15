0c3631d864eac8bc59a50ac2bac8510d04e299be
romdim <rom-dim@hotmail.com>
2014-11-15 13:54:34 +0200

    Set path constants

--- config/params.php
+++ config/params.php
@@ -1,5 +1,8 @@
 <?php
 
+Yii::setAlias('@rootDirectory', realpath(dirname(__FILE__).'/../'));
+Yii::setAlias('@apisDirectory', realpath(dirname(__FILE__).'/../apis/'));
+
 return [
     'adminEmail' => 'rom-dim@hotmail.com',
 	'api_url' => 'https://demo1.openi-ict.eu/',
