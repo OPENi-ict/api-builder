059c82fd759e1a0a268d0a8db4db263f0158fccc
romdim <rom-dim@hotmail.com>
2014-11-27 22:12:40 +0200

    Layout and Assets for Swagger
    Also correct folder for apis in params

---
+++ assets/SwaggerAsset.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class SwaggerAsset extends AssetBundle
+{
+	public $basePath = '@webroot';
+	public $baseUrl = '@web';
+	public $css = [
+		// Swagger-Specific
+		'css/swagger/reset.css',
+		'css/swagger/screen.css',
+		'http://fonts.googleapis.com/css?family=Droid+Sans:400,700',
+
+		'css/site.css',
+		'css/ripples.min.css',
+		'css/material-wfont.min.css',
+		'css/snackbar.min.css',
+	];
+	public $js = [
+		'js/ripples.min.js',
+		'js/material.min.js',
+		'js/snackbar.min.js',
+		// Swagger-Specific
+		'js/swagger/shred.bundle.js',
+		'js/swagger/jquery-1.8.0.min.js',
+		'js/swagger/jquery.slideto.min.js',
+		'js/swagger/jquery.wiggle.min.js',
+		'js/swagger/jquery.ba-bbq.min.js',
+		'js/swagger/handlebars-1.0.0.js',
+		'js/swagger/underscore-min.js',
+		'js/swagger/backbone-min.js',
+		'js/swagger/swagger.js',
+		'js/swagger/swagger-client.js',
+		'js/swagger/swagger-ui.min.js',
+		'js/swagger/highlight.7.3.pack.js',
+		'js/swagger/site.js'
+	];
+	public $depends = [
+		'yii\web\YiiAsset',
+		'yii\bootstrap\BootstrapAsset',
+	];
+}
---
+++ views/layouts/swagger.php
@@ -0,0 +1,89 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\SwaggerAsset;
+use app\widgets\Alert;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+SwaggerAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+	<meta charset="<?= Yii::$app->charset ?>"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="API Builder for OPENi">
+	<?= Html::csrfMetaTags() ?>
+	<title><?= Html::encode($this->title) ?></title>
+	<?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+<div class="wrap">
+	<?php
+	NavBar::begin([
+		'brandLabel' => '<img src="/api-builder/web/OPENi_logo.png">',
+		'brandUrl' => Yii::$app->homeUrl,
+		'options' => [
+			'class' => 'navbar-inverse navbar-default navbar-fixed-top',
+		],
+	]);
+	$menuItems = [
+		['label' => 'Home', 'url' => ['/site/index']],
+		['label' => 'About', 'url' => ['/site/about']],
+		['label' => 'Browse', 'url' => ['/apis/'], 'items' => [
+			['label' => 'APIs', 'url' => ['/apis/']],
+			['label' => 'Objects', 'url' => ['/objects/']]
+		]],
+		['label' => 'Create', 'url' => ['/apis/create'], 'items' => [
+			['label' => 'API', 'url' => ['/apis/create']],
+			['label' => 'Object', 'url' => ['/objects/create']]
+		]],
+		//['label' => 'Contact', 'url' => ['/site/contact']],
+	];
+	if (Yii::$app->user->isGuest) {
+		$menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+		$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+	} else {
+		$menuItems[] = [
+			'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+			'url' => ['/site/logout'],
+			'linkOptions' => ['data-method' => 'post']
+		];
+	}
+	echo Nav::widget([
+		'options' => ['class' => 'navbar-nav navbar-right'],
+		'items' => $menuItems,
+	]);
+	NavBar::end();
+	?>
+
+	<div class="container">
+		<?= Breadcrumbs::widget([
+			'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+		]) ?>
+		<?= Alert::widget() ?>
+		<?= $content ?>
+	</div>
+</div>
+
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">API Builder <?= date('Y') ?></p>
+		<!--        <p class="pull-right">--><?//= Yii::powered() ?><!--</p>-->
+	</div>
+</footer>
+
+<?php $this->endBody() ?>
+<script>
+	$(function() {
+		$.material.init();
+	});
+</script>
+</body>
+</html>
+<?php $this->endPage() ?>
