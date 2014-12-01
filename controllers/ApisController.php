253e9177a94c4d207ed15690905307f08f19d4b5
romdim <rom-dim@hotmail.com>
2014-12-01 18:50:58 +0200

    Fixed whole publish swagger for all APIs

--- controllers/ApisController.php
+++ controllers/ApisController.php
@@ -15,6 +15,7 @@ use yii\helpers\Url;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use Swagger\Swagger;
 
 /**
  * ApisController implements the CRUD actions for Apis model.
@@ -141,7 +142,9 @@ class ApisController extends Controller
 	public function actionPublish($id)
 	{
 		$api = $this->findModel($id);
-		$basePath = str_replace('/rebuildschema/publish', '', Url::canonical());
+
+		$basePathPart = explode('publish', Url::canonical());
+		$basePath = $basePathPart[0] . $api->name . '/';
 
 		$objects = Objects::findAll(['api' => $id]);
 
@@ -149,145 +152,174 @@ class ApisController extends Controller
 		$swaggerJSON->BuildJSON();
 		foreach ($objects as $object)
 		{
-			$swaggerJSON->BuildResource($api->version, '1.2', $basePath, $object->name);
 
 			$object->methods = explode(',', $object->methods);
 
-			foreach ($object->methods as $methodName)
-			{
-				// $methodParts[method_name, path]
-				$methodParts = explode(' ', $methodName);
-				$upper_method = strtoupper($methodParts[0]);
+			// If this Resource has Methods
+			if ($object->methods != [""]) {
+				$swaggerJSON->BuildResource($api->version, '1.2', $basePath, $object->name);
 
-				// $pathParts = [object, ({id}, connection)]
-				$pathParts = explode('/', $methodParts[1]);
-				$numOfPathParts = count($pathParts);
-				$swaggerJSON->BuildAPI($methodName, NULL);
+				foreach ($object->methods as $methodName) {
+					// $methodParts[method_name, path]
+					$methodParts = explode(' ', $methodName);
+					$upper_method = strtoupper($methodParts[0]);
 
-				$summary = '';
-				$nickname = '';
-				$returnType = '';
+					// $pathParts = [object, ({id}, connection)]
+					$pathParts = explode('/', $methodParts[1]);
+					$numOfPathParts = count($pathParts);
+					$swaggerJSON->BuildAPI($methodParts[1], NULL);
 
-				switch ($upper_method) {
-					case 'GET':
-						switch ($numOfPathParts) {
-							case 1:
-								$summary = 'Retrieve a list of ' . $pathParts[0];
-								$nickname = 'Get_all_' . $pathParts[0];
-								$returnType = 'list_' . $pathParts[0];
-								break;
-							case 2:
-								$summary = 'Retrieve one ' . $pathParts[0];
-								$nickname = 'Get_one_' . $pathParts[0];
-								$returnType = $pathParts[0];
-								break;
-							case 3:
-								$summary = 'Retrieve a list of ' . $pathParts[0] . ' ' . $pathParts[2];
-								$nickname = 'Get_all_' . $pathParts[2];
-								$returnType = 'list_' . $pathParts[2];
-						}
-						break;
-					case 'POST':
-						switch ($numOfPathParts) {
-							case 1:
-								$summary = 'Create a new ' . $pathParts[0] . ' object';
-								$nickname = 'Post_one_' . $pathParts[0];
-								$returnType = $pathParts[0];
-								break;
-							case 3:
-								$summary = 'Create a new ' . $pathParts[2] . 'as a cconection of ' . $pathParts[0];
-								$nickname = 'Post_one_' . $pathParts[2];
-								$returnType = $pathParts[2] . '_post';
-						}
-						break;
-					case 'PUT':
-						switch ($numOfPathParts) {
-							case 2:
-								$summary = 'Change a particular ' . $pathParts[0];
-								$nickname = 'Put_' . $pathParts[0];
-								$returnType = $pathParts[0];
-						}
-						break;
-					case 'DELETE':
-						switch ($numOfPathParts) {
-							case 1:
-								$summary = 'Delete all ' . $pathParts[0];
-								$nickname = 'Delete_all_' . $pathParts[0];
-								$returnType = 'list_' . $pathParts[0];
-								break;
-							case 2:
-								$summary = 'Delete one ' . $pathParts[0];
-								$nickname = 'Delete_one_' . $pathParts[0];
-								$returnType = 'boolean';
-								break;
-							case 3:
-								$summary = 'Delete all ' . $pathParts[2] . ' connections of ' . $pathParts[0];
-								$nickname = 'Delete_all_' . $pathParts[0];
-								$returnType = 'boolean';
-						}
-						break;
+					$summary = '';
+					$nickname = '';
+					$returnType = '';
+
+					switch ($upper_method) {
+						case 'GET':
+							switch ($numOfPathParts) {
+								case 1:
+									$summary = 'Retrieve a list of ' . $pathParts[0];
+									$nickname = 'Get_all_' . $pathParts[0];
+									$returnType = 'list_' . $pathParts[0];
+									break;
+								case 2:
+									$summary = 'Retrieve one ' . $pathParts[0];
+									$nickname = 'Get_one_' . $pathParts[0];
+									$returnType = $pathParts[0];
+									break;
+								case 3:
+									$summary = 'Retrieve a list of ' . $pathParts[0] . ' ' . $pathParts[2];
+									$nickname = 'Get_all_' . $pathParts[2];
+									$returnType = 'list_' . $pathParts[2];
+							}
+							break;
+						case 'POST':
+							switch ($numOfPathParts) {
+								case 1:
+									$summary = 'Create a new ' . $pathParts[0] . ' object';
+									$nickname = 'Post_one_' . $pathParts[0];
+									$returnType = $pathParts[0];
+									break;
+								case 3:
+									$summary = 'Create a new ' . $pathParts[2] . 'as a cconection of ' . $pathParts[0];
+									$nickname = 'Post_one_' . $pathParts[2];
+									$returnType = $pathParts[2] . '_post';
+							}
+							break;
+						case 'PUT':
+							switch ($numOfPathParts) {
+								case 2:
+									$summary = 'Change a particular ' . $pathParts[0];
+									$nickname = 'Put_' . $pathParts[0];
+									$returnType = $pathParts[0];
+							}
+							break;
+						case 'DELETE':
+							switch ($numOfPathParts) {
+								case 1:
+									$summary = 'Delete all ' . $pathParts[0];
+									$nickname = 'Delete_all_' . $pathParts[0];
+									$returnType = 'list_' . $pathParts[0];
+									break;
+								case 2:
+									$summary = 'Delete one ' . $pathParts[0];
+									$nickname = 'Delete_one_' . $pathParts[0];
+									$returnType = 'boolean';
+									break;
+								case 3:
+									$summary = 'Delete all ' . $pathParts[2] . ' connections of ' . $pathParts[0];
+									$nickname = 'Delete_all_' . $pathParts[0];
+									$returnType = 'boolean';
+							}
+							break;
+					}
+
+					$swaggerJSON->BuildOperation($upper_method, $summary, '', $returnType, $nickname);
+
+					if ($numOfPathParts > 1)
+						$swaggerJSON->BuildParameter('id', 'Primary key of resource', true, 'integer', 'body');
+					if ($upper_method == 'POST' or $upper_method == 'PUT')
+						$swaggerJSON->BuildParameter($pathParts[0], 'Model of resource', true, $pathParts[0] . '_post_put', 'query');
+
+					$swaggerJSON->CloseOperation();
+
+					$swaggerJSON->CloseAPI();
 				}
 
-				$swaggerJSON->BuildOperation($upper_method, $summary, '', $returnType, $nickname);
+				$swaggerJSON->CloseResource();
 
-				if ($numOfPathParts > 1)
-					$swaggerJSON->BuildParameter('id', 'Primary key of resource', true, 'integer', 'body');
-				if ($upper_method == 'POST' or $upper_method == 'PUT')
-					$swaggerJSON->BuildParameter($pathParts[0], 'Model of resource', true, $pathParts[0] . '_post_put', 'query');
+				// ALL THE DIFFERENT MODEL TYPES THAT NEED TO BE IMPLEMENTED
+				// Meta
+				// $pathParts[0] + Meta
+				// $pathParts[0] . '_post_put'
+				// $pathParts[2] . '_post'
+				// 'list_' . $pathParts[0]
+				// 'list_' . $pathParts[2]
+				// THE MODELS FOR THE CONNECTIONS HAVE TO BE BUILT WHEN THAT PARTICULAR OBJECT COMES IN
 
-				$swaggerJSON->CloseOperation();
+				$swaggerJSON->BuildModel('Meta');
+				$swaggerJSON->BuildProperty('previous', 'Uri of the previous page relative to the current page settings.', 'string');
+				$swaggerJSON->BuildProperty('next', 'Uri of the next page relative to the current page settings.', 'string');
+				$swaggerJSON->BuildProperty('total_count', 'Total items count for all the collection.', 'integer');
+				$swaggerJSON->BuildProperty('offset', 'Specify the offset to start displaying element on a page.', 'integer');
+				$swaggerJSON->BuildProperty('limit', 'Specify the number of element to display per page.', 'integer');
+				$swaggerJSON->CloseModel();
 
-				$swaggerJSON->CloseAPI();
-			}
+				$properties = Properties::findAll(['object' => $object->id]);
 
-			$swaggerJSON->CloseResource();
-
-			// ALL THE DIFFERENT MODEL TYPES THAT NEED TO BE IMPLEMENTED
-			// Meta
-			// $pathParts[0] + Meta
-			// $pathParts[0] . '_post_put'
-			// $pathParts[2] . '_post'
-			// 'list_' . $pathParts[0]
-			// 'list_' . $pathParts[2]
-			// THE MODELS FOR THE CONNECTIONS HAVE TO BE BUILT WHEN THAT PARTICULAR OBJECT COMES IN
-
-			$swaggerJSON->BuildModel('Meta');
-			$swaggerJSON->BuildProperty('previous', 'Uri of the previous page relative to the current page settings.', 'string');
-			$swaggerJSON->BuildProperty('next', 'Uri of the next page relative to the current page settings.', 'string');
-			$swaggerJSON->BuildProperty('total_count', 'Total items count for all the collection.', 'integer');
-			$swaggerJSON->BuildProperty('offset', 'Specify the offset to start displaying element on a page.', 'integer');
-			$swaggerJSON->BuildProperty('limit', 'Specify the number of element to display per page.', 'integer');
-			$swaggerJSON->CloseModel();
-
-			$properties = Properties::findAll(['object' => $object->id]);
-
-			$swaggerJSON->BuildModel($pathParts[0]);
-			foreach ($properties as $property)
-			{
-				$swaggerJSON->BuildProperty($property->name, $property->description, $property->type);
-			}
-			$swaggerJSON->BuildProperty('meta', '', 'Meta');
-			$swaggerJSON->CloseModel();
-
-			$swaggerJSON->BuildModel($pathParts[0] . '_post_put');
-			foreach ($properties as $property)
-			{
-				if ($property->name != 'id')
+				// $pathParts[0] + Meta
+				$swaggerJSON->BuildModel($object->name);
+				foreach ($properties as $property) {
 					$swaggerJSON->BuildProperty($property->name, $property->description, $property->type);
-			}
-			$swaggerJSON->CloseModel();
+				}
+				$swaggerJSON->BuildProperty('meta', '', 'Meta');
+				$swaggerJSON->CloseModel();
 
-			$swaggerJSON->BuildModel('list_' . $pathParts[0]);
-			$swaggerJSON->BuildProperty('meta', '', 'Meta');
-			$swaggerJSON->BuildProperty('meta', '', 'Meta');
-			$swaggerJSON->BuildProperty($property->name, $property->description, $property->type);
-			$swaggerJSON->CloseModel();
+				// $pathParts[0] . '_post_put'
+				$swaggerJSON->BuildModel($object->name . '_post_put');
+				foreach ($properties as $property) {
+					if ($property->name != 'id')
+						$swaggerJSON->BuildProperty($property->name, $property->description, $property->type);
+				}
+				$swaggerJSON->CloseModel();
+
+				// 'list_' . $pathParts[0]
+				$swaggerJSON->BuildModel('Objects_' . $object->name);
+				$swaggerJSON->BuildProperty($object->name, '', 'array', $object->name);
+				$swaggerJSON->CloseModel();
+
+				$swaggerJSON->BuildModel('list_' . $object->name);
+				$swaggerJSON->BuildProperty('meta', '', 'Meta');
+				$swaggerJSON->BuildProperty('objects', '', 'Objects_' . $object->name);
+				$swaggerJSON->CloseModel();
+			}
 		}
 		$swaggerJSON->CloseJSON();
 
 		$file = new FileManipulation();
-		$file->setFilename(ucfirst($this->_core) . '/' . $this->_core . '.php');
+		$file->setFilename(ucfirst($api->name) . '/' . $api->name . '.php');
+		$file->makeDirectory(ucfirst($api->name) . '/');
 		$file->write_file($swaggerJSON->getSwaggerJSON());
+
+
+		// Actually Publish the API
+		$_file = Yii::getAlias('@apisDirectory') . '/' . ucfirst($api->name);
+		$swagger = new Swagger($_file);
+
+		$link_parts = explode('/apis/publish', Url::canonical());
+		$actual_link = $link_parts[0];
+
+
+		$writeFiles = new FileManipulation();
+		$writeFiles->setFilename(ucfirst($api->name) . '/api-docs.json');
+		$writeFiles->write_file($swagger->getResourceList(array('output' => 'json', 'basePath' => $actual_link . '/api-docs/' . ucfirst($api->name))));
+
+		foreach($swagger->registry as $api_name => $api_resource)
+		{
+			$writeFiles->setFilename(ucfirst($api->name) . '/'. $api_name);
+			$writeFiles->write_file($swagger->getResource($api_name, array('output' => 'json')));
+		}
+
+		return $this->redirect(['swagger/index', 'url' => ucfirst($api->name)]);
 	}
 
     /**
