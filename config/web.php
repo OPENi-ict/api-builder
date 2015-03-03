1d3daa6139a1fcc7c613beab9f65e9fa28de7acb
romdim <rom-dim@hotmail.com>
2015-03-03 19:47:40 +0200

    Minified js and css

--- config/web.php
+++ config/web.php
@@ -29,6 +29,23 @@ $config = [
 		'assetManager' => [
 			'class' => 'yii\web\AssetManager',
 			'appendTimestamp' => true,
+			'bundles' => [
+				'yii\web\JqueryAsset' => [
+					'js' => [
+						YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+					]
+				],
+				'yii\bootstrap\BootstrapAsset' => [
+					'css' => [
+						YII_ENV_DEV ? 'css/bootstrap.css' :         'css/bootstrap.min.css',
+					]
+				],
+				'yii\bootstrap\BootstrapPluginAsset' => [
+					'js' => [
+						YII_ENV_DEV ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
+					]
+				]
+			],
 		],
         'errorHandler' => [
             'errorAction' => 'site/error',
