458e640197f5f6176318115132406f910dbb2da5
romdim <rom-dim@hotmail.com>
2015-03-09 02:26:13 +0200

    New migration for notif system
    Enriched 2 db tables, the following ones - for apis and users
    Updated their models as well

--- models/FollowUserApi.php
+++ models/FollowUserApi.php
@@ -3,60 +3,103 @@
 namespace app\models;
 
 use Yii;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "follow_user_api".
  *
  * @property integer $follower
  * @property integer $api
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $last_seen
+ * @property integer $changed_name
+ * @property string $changed_name_old_name
+ * @property integer $changed_descr
+ * @property integer $changed_version
+ * @property integer $changed_upvotes
+ * @property integer $changed_downvotes
+ * @property integer $changed_proposed
+ * @property integer $changed_published
+ * @property integer $changed_privacy
+ * @property integer $changed_objects_number
  *
  * @property Apis $api0
  * @property User $follower0
  */
 class FollowUserApi extends \yii\db\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return 'follow_user_api';
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return 'follow_user_api';
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['follower', 'api'], 'integer']
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['follower', 'api', 'created_at', 'updated_at', 'changed_name', 'changed_descr', 'changed_version', 'changed_upvotes', 'changed_downvotes', 'changed_proposed', 'changed_published', 'changed_privacy', 'changed_objects_number'], 'integer'],
+			[['created_at', 'updated_at'], 'required'],
+			[['last_seen'], 'safe'],
+			[['changed_name_old_name'], 'string', 'max' => 255]
+		];
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'follower' => 'Follower',
-            'api' => 'Api',
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors()
+	{
+		return [
+			[
+				'class' => TimestampBehavior::className(),
+				'updatedAtAttribute' => false,
+			]
+		];
+	}
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getApi0()
-    {
-        return $this->hasOne(Apis::className(), ['id' => 'api']);
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'follower' => 'Follower',
+			'api' => 'Api',
+			'created_at' => 'Created At',
+			'updated_at' => 'Updated At',
+			'last_seen' => 'Last Seen',
+			'changed_name' => 'Changed Name',
+			'changed_name_old_name' => 'Changed Name Old Name',
+			'changed_descr' => 'Changed Descr',
+			'changed_version' => 'Changed Version',
+			'changed_upvotes' => 'Changed Upvotes',
+			'changed_downvotes' => 'Changed Downvotes',
+			'changed_proposed' => 'Changed Proposed',
+			'changed_published' => 'Changed Published',
+			'changed_privacy' => 'Changed Privacy',
+			'changed_objects_number' => 'Changed Objects Number',
+		];
+	}
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getFollower0()
-    {
-        return $this->hasOne(User::className(), ['id' => 'follower']);
-    }
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getApi0()
+	{
+		return $this->hasOne(Apis::className(), ['id' => 'api']);
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getFollower0()
+	{
+		return $this->hasOne(User::className(), ['id' => 'follower']);
+	}
 }
--- models/FollowUserUser.php
+++ models/FollowUserUser.php
@@ -3,60 +3,95 @@
 namespace app\models;
 
 use Yii;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "follow_user_user".
  *
  * @property integer $follower
  * @property integer $followee
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $last_seen
+ * @property integer $changed_photo
+ * @property integer $changed_linkedin
+ * @property integer $changed_github
+ * @property string $created_api
+ * @property string $changed_upvotes_apis
+ * @property string $changed_downvotes_apis
  *
  * @property User $followee0
  * @property User $follower0
  */
 class FollowUserUser extends \yii\db\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return 'follow_user_user';
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return 'follow_user_user';
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['follower', 'followee'], 'integer']
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['follower', 'followee', 'created_at', 'updated_at', 'changed_photo', 'changed_linkedin', 'changed_github'], 'integer'],
+			[['created_at', 'updated_at'], 'required'],
+			[['last_seen'], 'safe'],
+			[['created_api', 'changed_upvotes_apis', 'changed_downvotes_apis'], 'string']
+		];
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'follower' => 'Follower',
-            'followee' => 'Followee',
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors()
+	{
+		return [
+			[
+				'class' => TimestampBehavior::className(),
+				'updatedAtAttribute' => false,
+			]
+		];
+	}
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getFollowee0()
-    {
-        return $this->hasOne(User::className(), ['id' => 'followee']);
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'follower' => 'Follower',
+			'followee' => 'Followee',
+			'created_at' => 'Created At',
+			'updated_at' => 'Updated At',
+			'last_seen' => 'Last Seen',
+			'changed_photo' => 'Changed Photo',
+			'changed_linkedin' => 'Changed Linkedin',
+			'changed_github' => 'Changed Github',
+			'created_api' => 'Created Api',
+			'changed_upvotes_apis' => 'Changed Upvotes Apis',
+			'changed_downvotes_apis' => 'Changed Downvotes Apis',
+		];
+	}
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getFollower0()
-    {
-        return $this->hasOne(User::className(), ['id' => 'follower']);
-    }
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getFollowee0()
+	{
+		return $this->hasOne(User::className(), ['id' => 'followee']);
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getFollower0()
+	{
+		return $this->hasOne(User::className(), ['id' => 'follower']);
+	}
 }
