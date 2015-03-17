e831f84a99f5c989b2a847f2a8fd7311afa55d81
romdim <rom-dim@hotmail.com>
2015-03-17 15:26:55 +0200

    Added 3 needed files, even in sample
    Unignore web/js/swagger/site.js
    Create Db and index Sample

---
+++ config/dbSample.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'class' => 'yii\db\Connection',
+	'dsn' => 'mysql:host=myHost;dbname=myDBName',
+	'username' => 'myUsername',
+	'password' => 'myPassword',
+	'charset' => 'utf8',
+];
