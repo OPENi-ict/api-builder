04239f892fea998cb51d0b66d70b294dc84d7518
romdim <rom-dim@hotmail.com>
2015-04-05 17:53:48 +0300

    9th try for travis

---
+++ config/webTest.php
@@ -0,0 +1,143 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+	'id' => 'api-builder',
+	'basePath' => dirname(__DIR__),
+	'bootstrap' => ['log'],
+	'components' => [
+		'request' => [
+			// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+			'cookieValidationKey' => 'ED3gwz0pE1evYZeGiENoyCiTL0rOLHqm',
+		],
+		'cache' => [
+			'class' => 'yii\caching\FileCache',
+		],
+		'user' => [
+			'identityClass' => 'app\models\User',
+			'enableAutoLogin' => true,
+		],
+		'authManager' => [
+			'class' => 'yii\rbac\DbManager',
+			'defaultRoles' => ['guest'],
+			'cache' => 'cache',   // this enables RBAC caching
+		],
+		'assetManager' => [
+			'class' => 'yii\web\AssetManager',
+			'appendTimestamp' => true,
+			'bundles' => [
+				'yii\web\JqueryAsset' => [
+					'js' => [
+						YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+					]
+				],
+				'yii\bootstrap\BootstrapAsset' => [
+					'css' => [
+						YII_ENV_DEV ? 'css/bootstrap.css' : 'css/bootstrap.min.css',
+					]
+				],
+				'yii\bootstrap\BootstrapPluginAsset' => [
+					'js' => [
+						YII_ENV_DEV ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
+					]
+				],
+				'romdim\bootstrap\material\BootMaterialCssAsset' => [
+					'css' => [
+						YII_ENV_DEV ? 'css/ripples.css' : 'css/ripples.min.css',
+						YII_ENV_DEV ? 'css/material-fullpalette.css' : 'css/material-fullpalette.min.css',
+					]
+				],
+				'romdim\bootstrap\material\BootMaterialJsAsset' => [
+					'js' => [
+						YII_ENV_DEV ? 'js/ripples.js' : 'js/ripples.min.js',
+						YII_ENV_DEV ? 'js/material.js' : 'js/material.min.js',
+					]
+				]
+			],
+		],
+		'errorHandler' => [
+			'errorAction' => 'site/error',
+		],
+		'urlManager' => [
+			'enablePrettyUrl' => true,
+			'showScriptName' => false,
+//			'rules' => [
+//				['class' => 'yii\rest\UrlRule', 'controller' => 'user'],
+//			],
+		],
+		'mailer' => [
+			'class' => 'yii\swiftmailer\Mailer',
+			// send all mails to a file by default. You have to set
+			// 'useFileTransport' to false and configure a transport
+			// for the mailer to send real emails.
+			'useFileTransport' => true,
+		],
+		'log' => [
+			'traceLevel' => YII_DEBUG ? 3 : 0,
+			'targets' => [
+				[
+					'class' => 'yii\log\FileTarget',
+					'levels' => ['error', 'warning'],
+				],
+			],
+		],
+		'db' => require(__DIR__ . '/dbTest.php'),
+		'sqlite' => require(__DIR__ . '/sqlite.php'),
+		//'db' => require(__DIR__ . '/sqlite.php'),
+	],
+	'modules' => [
+		'gridview' =>  [
+			'class' => '\kartik\grid\Module'
+			// enter optional module parameters below - only if you need to
+			// use your own export download action or custom translation
+			// message source
+			// 'downloadAction' => 'gridview/export/download',
+			// 'i18n' => []
+		],
+		'datecontrol' =>  [
+			'class' => 'kartik\datecontrol\Module',
+
+			// format settings for displaying each date attribute
+			'displaySettings' => [
+				'date' => 'd-m-Y',
+				'time' => 'H:i:s A',
+				'datetime' => 'd-m-Y H:i:s A',
+			],
+
+			// format settings for saving each date attribute
+			'saveSettings' => [
+				'date' => 'Y-m-d',
+				'time' => 'H:i:s',
+				'datetime' => 'Y-m-d H:i:s',
+			],
+
+			// automatically use kartik\widgets for each of the above formats
+			'autoWidget' => true,
+		],
+		'utility' => [
+			'class' => 'c006\utility\migration\Module',
+		],
+		'yii2images' => [
+			'class' => 'rico\yii2images\Module',
+			//be sure, that permissions ok
+			//if you cant avoid permission errors you have to create "images" folder in web root manually and set 777 permissions
+			'imagesStorePath' => 'images/store', //path to origin images
+			'imagesCachePath' => 'images/cache', //path to resized copies
+			'graphicsLibrary' => 'GD', //but really its better to use 'Imagick'
+			'placeHolderPath' => '@webroot/images/def_avatar.png', // if you want to get placeholder when image not exists, string will be processed by Yii::getAlias
+		],
+	],
+	'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+	// configuration adjustments for 'dev' environment
+	$config['bootstrap'][] = 'debug';
+	$config['modules']['debug'] = 'yii\debug\Module';
+
+	$config['bootstrap'][] = 'gii';
+	$config['modules']['gii']['class'] = 'yii\gii\Module';
+}
+
+return $config;
