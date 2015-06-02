ebbb0e6ccaac0d4798eb6b656bb714e21b743948
Romanos Tsouroplis <rom-dim@hotmail.com>
2015-06-02 18:06:00 +0300

    wadl fix 2

--- views/apis/view.php
+++ views/apis/view.php
@@ -202,7 +202,7 @@ foreach ($recommend['hits']['hits'] as $rec)
                 <?= Html::a(Html::img('@web/images/standards/hydra-cg.svg', ['height' => '20px', 'width' => '70px']), ['hydra', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
                 <?= Html::a(Html::img('@web/images/standards/raml-logo.jpg', ['height' => '34px', 'width' => '94px']), ['raml', 'id' => $model->id], ['class' => 'btn btn-primary img-standards']) ?>
                 <?= Html::a(Html::img('@web/images/standards/api-blueprint.jpg', ['height' => '34px', 'width' => '100px']), ['blueprint', 'id' => $model->id], ['class' => 'btn btn-primary img-standards']) ?>
-                <?= Html::a('&nbsp;&nbsp;&nbsp;WADL&nbsp;&nbsp;&nbsp;', ['wadl/', 'id' => $model->id], ['class' => 'btn btn-primary', 'height' => '34px', 'width' => '94px']) ?>
+                <?= Html::a('&nbsp;&nbsp;&nbsp;WADL&nbsp;&nbsp;&nbsp;', ['wadl', 'id' => $model->id], ['class' => 'btn btn-primary', 'height' => '34px', 'width' => '94px']) ?>
             <?php endif; ?>
         </h1>
     </div>
